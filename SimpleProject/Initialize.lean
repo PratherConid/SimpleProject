c67375b5d5c6212ad79d9b38cde164c88a979d08
IndPrinciple <syxx2013050321@163.com>
2023-07-08 18:19:47 -0400

    second

--- SimpleProject/Initialize.lean
+++ SimpleProject/Initialize.lean
@@ -3,3 +3,5 @@ def builtin_init_test1 : IO Unit := IO.println "builtin_init_test1"
 
 @[init]
 def init_test2 : IO Unit := IO.println "init_test_2"
+
+builtin_initialize builtinit_test3 : Unit ← IO.println "builtinit_test3"
